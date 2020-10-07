94e6cd3f70b6cbf08606aa7e0b7b16746a19bf95
edgar <alexdta@gmail.com>
2020-10-07 13:17:46 -0600

    Double a Texto

---
+++ doubleTexto/MainForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace doubleTexto
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.cdrNumer = new System.Windows.Forms.TextBox();
+            this.cdrTexto = new System.Windows.Forms.TextBox();
+            this.lblNum = new System.Windows.Forms.Label();
+            this.lblTxt = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cdrNumer
+            // 
+            this.cdrNumer.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cdrNumer.Location = new System.Drawing.Point(83, 15);
+            this.cdrNumer.Margin = new System.Windows.Forms.Padding(4);
+            this.cdrNumer.Name = "cdrNumer";
+            this.cdrNumer.Size = new System.Drawing.Size(344, 23);
+            this.cdrNumer.TabIndex = 0;
+            // 
+            // cdrTexto
+            // 
+            this.cdrTexto.AcceptsReturn = true;
+            this.cdrTexto.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cdrTexto.Location = new System.Drawing.Point(83, 48);
+            this.cdrTexto.Margin = new System.Windows.Forms.Padding(4);
+            this.cdrTexto.Multiline = true;
+            this.cdrTexto.Name = "cdrTexto";
+            this.cdrTexto.Size = new System.Drawing.Size(455, 114);
+            this.cdrTexto.TabIndex = 1;
+            // 
+            // lblNum
+            // 
+            this.lblNum.AutoSize = true;
+            this.lblNum.Location = new System.Drawing.Point(16, 18);
+            this.lblNum.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNum.Name = "lblNum";
+            this.lblNum.Size = new System.Drawing.Size(58, 17);
+            this.lblNum.TabIndex = 2;
+            this.lblNum.Text = "Numero";
+            // 
+            // lblTxt
+            // 
+            this.lblTxt.AutoSize = true;
+            this.lblTxt.Location = new System.Drawing.Point(16, 52);
+            this.lblTxt.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTxt.Name = "lblTxt";
+            this.lblTxt.Size = new System.Drawing.Size(43, 17);
+            this.lblTxt.TabIndex = 3;
+            this.lblTxt.Text = "Texto";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(436, 11);
+            this.btnConvert.Margin = new System.Windows.Forms.Padding(4);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(100, 28);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "Convertir";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.BtnConvert_Click);
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.btnConvert;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 188);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.lblTxt);
+            this.Controls.Add(this.lblNum);
+            this.Controls.Add(this.cdrTexto);
+            this.Controls.Add(this.cdrNumer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "doubleTexto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+		private System.Windows.Forms.Button btnConvert;
+		private System.Windows.Forms.TextBox cdrTexto;
+		 
+		private	System.Windows.Forms.Label lblTxt;
+		private System.Windows.Forms.Label lblNum;
+		
+			private System.Windows.Forms.TextBox cdrNumer;
+    }
+}
---
+++ doubleTexto/MainForm.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace doubleTexto
+{
+
+    public partial class MainForm : Form
+    {
+
+        Dictionary<double, string> cifras = new Dictionary<double, string>()
+        {
+            { 0, "cero" },
+            { 1, "uno" },
+            { 2, "dos" },
+            { 3, "tres" },
+            { 4, "cuatro" },
+            { 5, "cinco" },
+            { 6, "seis" },
+            { 7, "siete" },
+            { 8, "ocho" },
+            { 9, "nueve" },
+            { 10, "diez" },
+            { 11, "once" },
+            { 12, "doce" },
+            { 13, "trece" },
+            { 14, "catorce" },
+            { 15, "quince" },
+            { 20, "veinte" },
+            { 30, "treinta" },
+            { 40, "cuarenta" },
+            { 50, "cincuenta" },
+            { 60, "sesenta" },
+            { 70, "setenta" },
+            { 80, "ochenta" },
+            { 90, "noventa" },
+            { 100, "cien" },
+            { 500, "quinientos" },
+            { 700, "setecientos" },
+            { 900, "novecientos" },
+            { 1000, "mil"},
+            { 1000000, "un millón"},
+            { 1000000000000, "un billón"}
+        };
+
+        #region Constructor
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Metodos
+
+        private string cifraLetras(double pValor)
+        {
+            double lCifra = Math.Truncate(pValor);
+
+            string lResultado = numeroTexto(lCifra);
+
+            return lResultado;
+        }
+
+        private string numeroTexto(double pValor)
+        {
+            string lNum2Text = "";
+
+            try
+            {
+                if (pValor >= 0 && pValor <= 15)
+                {
+                    //0 - 15
+                    lNum2Text = cifras[pValor];
+                }
+                else if (pValor < 20)
+                {
+                    //16 - 19
+                    lNum2Text = "dieci" + numeroTexto(pValor - 10);
+                }
+                else if (pValor == 20)
+                {
+                    //20
+                    lNum2Text = cifras[pValor];
+                }
+                else if (pValor < 30)
+                {
+                    //21-29
+                    lNum2Text = "veinti" + numeroTexto(pValor - 20);
+                }
+                else if (pValor < 100 && pValor % 10 == 0)
+                {
+                    lNum2Text = cifras[pValor];
+                }
+                else if (pValor < 100 && pValor % 10 != 0)
+                {
+                    double a = (Math.Truncate(pValor / 10)) * 10;
+                    double b = pValor % 10;
+                    lNum2Text = cifras[a] + " y " + cifras[b];
+                }
+                else if (pValor == 100)
+                {
+                    lNum2Text = cifras[pValor];
+                }
+                else if (pValor < 200)
+                {
+                    lNum2Text = "ciento " + numeroTexto(pValor - 100);
+                }
+                else if (pValor < 1000 && (new List<double> { 200, 300, 400, 600, 800 }).Contains(pValor))
+                {
+                    lNum2Text = numeroTexto(Math.Truncate(pValor / 100)) + "cientos";
+                }
+                else if (pValor < 1000 && (new List<double> { 500, 700, 900 }).Contains(pValor))
+                {
+                    lNum2Text = cifras[pValor];
+                }
+                else if (pValor < 1000)
+                {
+                    lNum2Text = numeroTexto(Math.Truncate(pValor / 100) * 100) + " " + numeroTexto(pValor % 100);
+                }
+                else if (pValor == 1000)
+                {
+                    lNum2Text = cifras[pValor];
+                }
+                else if (pValor < 2000)
+                {
+                    double a = 1000;
+                    double b = pValor % a;
+                    lNum2Text = cifras[a] + " " + numeroTexto(b);
+                }
+                else if (pValor < 1000000)
+                {
+                    double a = Math.Truncate(pValor / 1000);
+                    double b = pValor % 1000;
+
+                    lNum2Text = numeroTexto(a) + " " + cifras[1000];
+
+                    if ((pValor % 1000) > 0)
+                    {
+                        lNum2Text = lNum2Text + " " + numeroTexto(b);
+                    }
+                }
+                else if (pValor == 1000000)
+                {
+                    lNum2Text = cifras[pValor];
+                }
+                else if (pValor < 2000000)
+                {
+                    double a = 1000000;
+                    double b = pValor % a;
+                    lNum2Text = cifras[a] + " " + numeroTexto(b);
+                }
+                else if (pValor < 1000000000000)
+                {
+                    lNum2Text = numeroTexto(Math.Truncate(pValor / 1000000)) + " millones ";
+
+                    if ((pValor - Math.Truncate(pValor / 1000000) * 1000000) > 0)
+                    {
+                        lNum2Text = lNum2Text + numeroTexto(pValor - Math.Truncate(pValor / 1000000) * 1000000);
+                    }
+                }
+                else if (pValor == 1000000000000)
+                {
+                    lNum2Text = cifras[pValor];
+                }
+                else if (pValor < 2000000000000)
+                {
+                    lNum2Text =
+                        cifras[1000000000000] + " " + numeroTexto(pValor - Math.Truncate(pValor / 1000000000000) * 1000000000000);
+                }
+                else
+                {
+                    lNum2Text = numeroTexto(Math.Truncate(pValor / 1000000000000)) + " billones";
+
+                    if ((pValor - Math.Truncate(pValor / 1000000000000) * 1000000000000) > 0)
+                    {
+                        lNum2Text = lNum2Text + " " + numeroTexto(pValor - Math.Truncate(pValor / 1000000000000) * 1000000000000);
+                    }
+                }
+            }
+            catch (Exception lExcp)
+            {
+                MessageBox.Show(lExcp.Message);
+            }
+
+            return lNum2Text;
+        }
+
+        #endregion
+
+        #region Botones
+
+        void BtnConvert_Click(object sender, EventArgs e)
+        {
+            this.cdrTexto.Clear();
+            this.cdrTexto.Text += cifraLetras(Convert.ToDouble(this.cdrNumer.Text));
+        }
+
+        #endregion
+
+    }
+}
